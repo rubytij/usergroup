367f9ce19b1077e4d87c86fab64c094f68ce5a0b
aalvarado <adana@amcoonline.net>
2012-05-02 15:40:19 -0700

    debug false, precompile on

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module UserGroup
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += %w/application.css Markdown.Editor.css/
   end
 end
