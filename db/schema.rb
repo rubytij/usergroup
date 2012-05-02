fe4647a0dbe6bb9c7f1a34035e5510a7a3e3ad1d
Adan Alvarado <adan.alvarado7@gmail.com>
2012-05-01 22:04:41 -0700

    redoing migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120428001853) do
+ActiveRecord::Schema.define(:version => 20120427234124) do
 
   create_table "posts", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
     t.string   "name"
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
