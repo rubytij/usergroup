f10a746ac3fe358ecc9329b04120b662365eab37
Adan Alvarado <adan.alvarado7@gmail.com>
2012-05-01 21:57:17 -0700

    cleaned commit from unused files

--- db/migrate/20120428001853_add_attributes_to_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddAttributesToPosts < ActiveRecord::Migration
-  def change
-    change_table :posts do |t|
-      t.string :name
-      t.string :title
-      t.text :content
-    end
-  end
-end
