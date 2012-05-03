d31e25290b0df5ada6d2084eae56a20d283323d7
Enrique Vidal <enrique@cloverinteractive.com>
2012-05-02 22:35:43 -0700

    Check authentication on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, :only => [ :new, :create ]
+
   def show
     user  = User.find params[:user_id]
     @post = user.posts.find params[:id]
