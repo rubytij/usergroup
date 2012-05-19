a970b76f2ef5788e5a816ccd8e14b85bc3b661d8
Enrique Vidal <enrique@cloverinteractive.com>
2012-05-19 01:42:09 -0700

    Make rspec work with capybara

--- spec/requests/posts_index_layout_spec.rb
+++ spec/requests/posts_index_layout_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
 
 describe 'PostsController#index layout' do
-  before :each do
-    @user = Factory.create :user
-    @post = Factory.create :post, :user => @user
-  end
+  before { @post = Factory.create :post }
 
   describe 'layout' do
     it 'should show could tag and avatar' do
@@ -12,27 +9,21 @@ describe 'PostsController#index layout' do
       page.should have_css( '.tag_cloud' )
       page.should have_css( '.avatar' )
     end
+  end
 
+  describe 'comments' do
     it 'should show disqus elements', :js => true do
-      visit user_post_path(@user.id, @post.id)
-      puts page.html
-      page.should have_css( '#disqus_thread' )
-      page.should have_css( '#dsq-comments-title' )
+      visit user_post_path( @post.user, @post )
+
+      find('#disqus_thread')
+      page.should have_css( '#dsq-content-stub' )
+      page.should have_css( '#dsq-content' )
+
+      find( '#dsq-comments' )
       page.should have_css( '#dsq-comments' )
       page.should have_css( '#dsq-new-post' )
-    end
-  end
-  describe 'create a new comment' do
-    before { visit user_post_path(@user.id, @post.id) }
 
-    describe 'with empty message' do
-      it "should show a error message", :js => true do
-        page.should_not have_css('#dsq-popup-message')
-        #fill_in( "dsq-textarea-wrapper", "foo" )
-        click_button( "dsq-post-button" )
-        page.should have_css('#dsq-popup-message')
-        page.should has_content?("Please enter a comment to post")
-      end
+      find('#dsq-textarea-wrapper')
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,16 +12,23 @@ Capybara.javascript_driver = :webkit
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   # ## Mock Framework
   config.mock_with :mocha
   config.color_enabled = true
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
