ea5c5ae3703711e5629b0729b632b541e79ba105
javierg <javierg@amcoonline.net>
2012-05-19 15:57:53 -0700

    cap files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,30 @@
-set :application, "user_group"
+require 'bundler/capistrano'
+
+set :application, "usergroup"
 set :scm, :git
 set :repository, `git config --get remote.origin.url`.chomp
 set :branch, "master"
-
-set :deploy_to, "/var/www/apps/#{application}"
-
+set :user, "deployer"
+set :ssh_name, "live.rt"
+ssh_options[:forward_agent] = true
+set :deploy_to, "/home/#{ user }/srv/app/#{application}"
+set :deploy_via, :remote_cache
 set :rails_env, ( ENV['RAILS_ENV'] = 'production' )
-set :ssh_name, "127.0.0.1"
+
+role :app, "live.rt"
+role :web, "live.rt"
+role :db, "live.rt", :primary => true
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  task :restart, :roles => :app do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+before 'deploy:restart' do
+  run "cd #{release_path} && bundle exec rake RAILS_ENV=#{rails_env} config:generate"
+end
+before 'deploy:restart', 'deploy:migrate'
+after :deploy, 'deploy:cleanup'
