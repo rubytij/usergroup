9d194138e7f2a949061cdce5a0ec4cde1c417478
Enrique Vidal <enrique@cloverinteractive.com>
2012-05-20 10:35:01 -0700

    Add helper specs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,8 +2,17 @@ require "spec_helper"
 
 describe ApplicationHelper do
   describe "markdown" do
+    before :each do
+      @markdown       = "hello\nworld `Code sample`"
+      @expected_html  = "<p>hello<br>\nworld <code>Code sample</code></p>\n"
+    end
+
     it "should return html safe" do
-      helper.markdown("foo").html_safe?.should be_true
+      helper.markdown( @markdown ).should be_html_safe
+    end
+
+    it "should turn markdown to html" do
+      helper.markdown( @markdown ).should eql( @expected_html )
     end
   end
 end
