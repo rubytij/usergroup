86a12e9a58cd7867da35ed8edadbb7233e5ddfd7
aalvarado <adana@amcoonline.net>
2012-06-15 15:29:58 -0700

    Added model scopes

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,7 @@ class Page < ActiveRecord::Base
   extend FriendlyId
 
   symbolize :section, :in => [ :home, :about, :contact, :meetings ], :scopes => true, :methods => true
+  symbolize :main_page, :scopes => true, :methods => true
 
   validates :name, :presence => true, :uniqueness => true
   validates :title, :content, :section, :presence => true
@@ -9,4 +10,7 @@ class Page < ActiveRecord::Base
   attr_accessible :name, :title, :content, :section
 
   friendly_id :name, :use => :slugged
+
+  scope :latest, order( 'created_at DESC' )
+  scope :latest_main, latest.where( :main_page => true )
 end
