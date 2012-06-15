e383d13b8d28ae1d8c2760d576172b3d1637792b
aalvarado <adana@amcoonline.net>
2012-06-15 16:04:28 -0700

    Added main_page method

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,16 +1,26 @@
 class Page < ActiveRecord::Base
   extend FriendlyId
 
+  before_save :check_main_page
   symbolize :section, :in => [ :home, :about, :contact, :meetings ], :scopes => true, :methods => true
-  symbolize :main_page, :scopes => true, :methods => true
 
   validates :name, :presence => true, :uniqueness => true
   validates :title, :content, :section, :presence => true
 
-  attr_accessible :name, :title, :content, :section
+  attr_accessible :name, :title, :content, :section, :main_page
 
   friendly_id :name, :use => :slugged
 
   scope :latest, order( 'created_at DESC' )
-  scope :latest_main, latest.where( :main_page => true )
+
+  def self.main_page
+    where( :main_page => true ).first
+  end
+
+  def check_main_page
+    if main_page?
+      page = Page.where("main_page = ?", true).first
+      page.update_attributes(:main_page => false) if page.present? && page != self
+    end
+  end
 end
