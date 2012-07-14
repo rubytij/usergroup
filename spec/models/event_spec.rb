8acf68aebfde98c2df41c4785897aec396d46473
aalvarado <adana@amcoonline.net>
2012-07-13 17:12:14 -0700

    added events

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Event do
+  before :each do
+    @user = Factory.create :user
+  end
+
+  describe "name" do
+    it "must be present" do
+      Factory.build( :event, :name => nil ).should be_invalid
+    end
+  end
+
+  describe "user" do
+    it "must have a user" do
+      Factory.build( :event, :user_id => nil ).should be_invalid
+    end
+  end
+
+  describe "start date and end date" do
+    it "end must be greater than start" do
+      Factory.build( :event, :start_date => DateTime.now + 1.day, :end_date => DateTime.now - 1.day ).should be_invalid
+    end
+  end
+
+  describe "description" do
+    it "must be present" do
+      Factory.build( :event, :description => nil ).should be_invalid
+    end
+  end
+end
