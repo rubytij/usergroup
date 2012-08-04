9e4af08e931aa58faf2203f7a5a305d940f3850d
aalvarado <adan.alvarado7@gmail.com>
2012-08-03 17:06:04 -0700

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 %w/home about contact meetings/.each do |section|
-  Page.create! :name => "#{ section } page", :section => section, :title => "#{ section } page title", :content => "This is a #{ section } page."
+  Page.create! :name => "main", :section => section, :title => "#{ section } page title", :content => "This is a #{ section } page."
 end
