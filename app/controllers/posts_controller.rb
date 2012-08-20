5c084a37f59341cd77fb8e230ad85ba943bfca99
Enrique Vidal <enrique@cloverinteractive.com>
2012-08-19 22:43:18 -0700

    Remove N+1 and translations problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :find_user_with_params, :only => [ :show, :index ]
 
   def index
-    @posts = Post.user_filtered( @user, params[:tag] ).paginate :page => params[:page], :per_page => 10
+    @posts = Post.user_filtered( @user, params[:tag] ).includes(:user).paginate :page => params[:page], :per_page => 10
   end
 
   def show
