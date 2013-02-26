7ff7a369cbe5daf174d313fd49c898699612959c
Adan Alvarado <adan.alvarado7@gmail.com>
2013-02-26 09:25:54 -0800

    Change deploy information

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
-set :user,        'deployer'
+set :user,        'w'
 set :application, 'usergroup'
 set :deploy_to,   "/home/#{user}/apps/#{application}"
 set :deploy_via,  :remote_cache
 set :deploy_env,  :production
-set :ssh_name,    'rubytij.org'
+set :ssh_name,    '198.211.98.157'
 
 server ssh_name, :app, :web, :db, :primary => true
